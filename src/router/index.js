d610b2defbc4bec96bece27723adf7ef987e7259
独步万里 <1874220706@qq.com>
2025-02-06 23:54:17 +0800

    111

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,7 @@
 // router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import JobIndex from '../views/JobIndex.vue'
-import AddJob from '../components/AddJob.vue'
 import TriggerIndex from '../views/TriggerIndex.vue'
-import onloadtrigger from '../components/onloadtrigger.vue'
 import test from '../components/test.vue'
 
 const routes = [
@@ -24,19 +22,9 @@ const routes = [
     component: JobIndex
   },
   {
-    path: '/AddJob',
-    name: 'AddJob',
-    component: AddJob
-  },
-  {
     path: '/TriggerIndex',
     name: 'TriggerIndex',
     component: TriggerIndex
-  },
-  {
-    path: '/onloadtrigger',
-    name: 'onloadtrigger',
-    component: onloadtrigger
   },
 ]
 
